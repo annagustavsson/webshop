eb9a5fa0fe53bc5cc44423d980c19b6a6c240da1
annagustavsson <annalovisaellen@gmail.com>
2021-02-05 23:00:44 +0100

    add buttons for quantityQunter

--- src/components/pages/DisplayPrint/DisplayPrint.jsx
+++ src/components/pages/DisplayPrint/DisplayPrint.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import image from "../../../resources/images/ystad.png";
-import classes from "../DisplayPrint/displayPrint.module.css"
+import classes from "../DisplayPrint/displayPrint.module.css";
 
-import BwButton from "../../../components/general/Buttons/BwButton/BwButton"
+import BwButton from "../../../components/general/Buttons/BwButton/BwButton";
+import Quantitycounter from "../../general/Quantitycounter/Quantitycounter";
 
 const DisplayPrint = () => {
     return (
         <div className={classes.displayContainer}>
+            <div>
+                <BwButton text={"-"}/>
+                <div>1</div>
+                <BwButton text={"+"}/>
+            </div>
              <img src={image} alt="image" />
              <div className={classes.buttonContainer}>
                  <BwButton text={"Buy know"}/>
